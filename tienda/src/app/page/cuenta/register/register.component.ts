16b02aa5b376f89cb314f013413ccb8d9eba7545
lionezel <lionezelfts@gmail.com>
2023-08-23 08:01:22 -0500

    toast de register

--- tienda/src/app/page/cuenta/register/register.component.ts
+++ tienda/src/app/page/cuenta/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ClienteService } from 'src/app/services/cliente.service';
 
 declare var iziToast: any;
@@ -12,7 +13,10 @@ export class RegisterComponent {
   public register_user: any = {};
   public new_user: any = {};
 
-  constructor(private _clienteServices: ClienteService) {}
+  constructor(
+    private _clienteServices: ClienteService,
+    private _router: Router
+  ) {}
 
   register(registerForm: any) {
     if (registerForm.valid) {
@@ -34,6 +38,16 @@ export class RegisterComponent {
             message: response.message,
           });
         } else {
+          this.new_user = response.data;
+          iziToast.show({
+            title: 'SUCCESS',
+            titleColor: '#1DC74C',
+            color: '#FFF',
+            class: 'text-success',
+            position: 'topRight',
+            message: 'Se ha registrado correctamente',
+          });
+          this._router.navigate(['/login']);
         }
 
         console.log(response);
