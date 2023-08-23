a8355834b2b9d45580ab92e8b5c48752242daa8d
lionezel <lionezelfts@gmail.com>
2023-08-23 07:56:57 -0500

    registador funcionando

--- tienda/src/app/page/cuenta/register/register.component.ts
+++ tienda/src/app/page/cuenta/register/register.component.ts
@@ -1,23 +1,43 @@
 import { Component } from '@angular/core';
+import { ClienteService } from 'src/app/services/cliente.service';
+
+declare var iziToast: any;
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  public register_user: any = {}
+  public register_user: any = {};
+  public new_user: any = {};
+
+  constructor(private _clienteServices: ClienteService) {}
 
   register(registerForm: any) {
-    if(registerForm.valid){
-     
+    if (registerForm.valid) {
       let data = {
-        names: this.register_user.names,
-        last_name: this.register_user.last_name,
+        nombres: this.register_user.nombres,
+        apellido: this.register_user.apellido,
         email: this.register_user.email,
         password: this.register_user.password,
-      }
-    }
+      };
 
+      this._clienteServices.register_cliente(data).subscribe((response) => {
+        if (response.data === undefined) {
+          iziToast.show({
+            title: 'ERROR',
+            titleColor: '#ff0000',
+            color: '#FFF',
+            class: 'text-danger',
+            position: 'topRight',
+            message: response.message,
+          });
+        } else {
+        }
+
+        console.log(response);
+      });
+    }
   }
 }
